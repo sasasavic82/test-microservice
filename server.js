185e3c705966a9b9942219c8019625eb16221a69
Savic <Sasa.Savic@team.telstra.com>
2016-08-30 20:49:47 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8088;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello from World Sash!\n');
+});
+
+app.listen(PORT);
