27fabf1538255fb4ca467bb5f00e8f9705745386
Savic <Sasa.Savic@team.telstra.com>
2016-09-13 10:43:40 +1000

    lots of changes

--- server.js
+++ server.js
@@ -1,14 +1,48 @@
 'use strict';
 
-const express = require('express');
+var app = require('express')(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server);
 
-// Constants
 const PORT = 8088;
 
-// App
-const app = express();
+var connections = [], locations = [];
+
+server.listen(PORT);
+
 app.get('/', function (req, res) {
-  res.send('Hello from World Sash and Fawad!!!!\n');
+  res.send('Hey!');
 });
 
-app.listen(PORT);
+app.post('/location', function (req, res) {
+  addLocation(req.body.lon, req.body.lat);
+});
+
+io.on('connection', function (socket) {
+  connectons.push(socket);
+  
+  console.log('Connection added:');
+  console.log(socket);
+
+  socket.on('disconnect', function() {
+    removeConnection(socket);
+  })
+});
+
+function removeConnection(socket) {
+  var idx = connections.indexOf(socket);
+  if(idx > -1)
+    connections.splice(idx, 1);
+}
+
+function addLocation(lon, lat) {
+  if(lon && lat) {
+    var loc = { lon: lon, lat: lat }
+    locations.push(loc);
+
+    connections.forEach(function(socket) {
+      socket.emit('location.update', loc)
+    })
+
+  }
+}
